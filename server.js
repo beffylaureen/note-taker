6cfde28a192662dc56a67d41cefc6a2c071d160d
B Laureen <beffylaureen@me.com>
2023-08-04 14:44:43 -0500

    commit 08042023 1444

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require ("fs");
 const path = require ("path");
 const { uuid } = require('uuidv4');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
